59c3afb18c53a1dc22a2acd07693ab1bf3704de6
flour <99916123+Flour510@users.noreply.github.com>
2024-06-11 18:13:55 -0700

    Work Shop

--- car-dealership.sql
+++ car-dealership.sql
@@ -2,59 +2,46 @@ DROP DATABASE IF EXISTS car_dealership;
 CREATE DATABASE car_dealership;
 USE car_dealership;
 
--- Note from Tiffany
--- CREATE TABLE Persons (
--- Personid int NOT NULL AUTO_INCREMENT,
--- LastName varchar(255) NOT NULL,
--- FirstName varchar(255),
--- Age int,
--- PRIMARY KEY (Personid)
--- );
-
 CREATE TABLE dealerships (
-	dealership_id int auto_increment primary key,
-	name varchar(50),
-	address varchar(50),
-	phone varchar(12)
+	dealership_id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50),
+	address VARCHAR(50),
+	phone VARCHAR(12)
 );
 INSERT INTO dealerships (name, address, phone)
 VALUES ('Flower Power Motors', '444 East Oakland', '510-444-4444');
 
 CREATE TABLE vehicles (
-	vin int primary key,
-	make varchar(20) NOT NULL,
-	model varchar(20) NOT NULL,
-	color varchar(20) NOT NULL,
-	price decimal(7,2) NOT NULL,
-	sold boolean NOT NULL
+	vin INT PRIMARY KEY,
+	make VARCHAR(20) NOT NULL,
+	model VARCHAR(20) NOT NULL,
+	color VARCHAR(20) NOT NULL,
+	price DECIMAL(7,2) NOT NULL,
+	sold BOOLEAN NOT NULL
 );
 INSERT INTO vehicles
-VALUES (123, 'volkswagen', 'beetle', 'pink', 3000, true);
-
--- Insert into the table we created 
--- Insert into dealerships(default,'Hello','Testing','New table');
--- Left off here with Tiffany
+VALUES (123, 'Volkswagen', 'Beetle', 'Pink', 3000, true);
 
 CREATE TABLE inventory (
 	dealership_id INT,
 	vin INT,
-    primary KEY (dealership_id, vin),
-    foreign KEY (dealership_id) REFERENCES dealerships(dealership_id),
-    foreign KEY (vin) REFERENCES vehicles(vin)    
+    PRIMARY KEY (dealership_id, vin),
+    FOREIGN KEY (dealership_id) REFERENCES dealerships(dealership_id),
+    FOREIGN KEY (vin) REFERENCES vehicles(vin)    
 );
 INSERT INTO inventory
 VALUES (1, 123);
 
-CREATE TABLE sales_contracts (
-	id int auto_increment primary key, 
-    vin int,
-    customer_name varchar(20),
-    dealership_id int,
+CREATE TABLE sales_contract (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    vin INT,
+    customer_name VARCHAR(20),
+    dealership_id INT,
     sale_date DATE,
     FOREIGN KEY (dealership_id) REFERENCES dealerships(dealership_id),
 	FOREIGN KEY (vin) REFERENCES vehicles(vin) 
 );
-INSERT INTO sales_contracts (vin, customer_name, dealership_id, sale_date)
+INSERT INTO sales_contract (vin, customer_name, dealership_id, sale_date)
 VALUES (123, 'Joe', 1, '2024-01-01');
 
 
--- tests.sql
+++ tests.sql
@@ -1,5 +1,3 @@
--- Tests
-
 -- Test 1
 SELECT *  
 FROM dealerships
@@ -9,8 +7,8 @@ FROM dealerships
 SELECT v.* 
 FROM vehicles v 
 JOIN inventory i ON v.vin = i.vin
-WHERE i.dealership_id = (SELECT dealership_id FROM dealerships WHERE NAME = 'Flower Power Motors') 
-; -- sub query 
+WHERE i.dealership_id = (SELECT dealership_id FROM dealerships WHERE NAME = 'Flower Power Motors') -- sub query 
+;
 
 -- Test 3
 SELECT * 
@@ -27,12 +25,11 @@ WHERE dealership_id = (SELECT dealership_id FROM inventory WHERE vin = 123)
 -- Test 5
 SELECT * 
 FROM dealerships 
-WHERE dealership_id IN (SELECT dealership_id FROM vehicles WHERE make = 'volkswagen')
+WHERE dealership_id IN (SELECT dealership_id FROM vehicles WHERE make = 'Volkswagen')
 ;
 
 -- Test 6 
 SELECT * 
 FROM sales_contract WHERE dealership_id = (SELECT dealership_id FROM dealerships WHERE dealership = 'Flower Power Motors')
-AND sale_date BETWEEN '2024-01-01'
-AND '2023-01-01'
+AND sale_date BETWEEN '2024-01-01' AND '2023-01-01'
 ;
